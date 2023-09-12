c1cd262cf47eaa9b6f3f6db3c6116661d71e0084
o7q2ab <o7q2ab@gmail.com>
2023-09-12 18:11:49 +0300

    Initial commit (3)

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("✨ gofun ✨")
+	if err := run(); err != nil {
+		fmt.Printf("❌ error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	return nil
 }
